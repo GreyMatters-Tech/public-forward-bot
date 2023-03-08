a74ff18797919a610874e5588df65eb3674e273f
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-03-08 23:15:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "23640300"))
+    API_HASH = os.environ.get("API_HASH", "b43066e201bfc457ad566e623fd74dab")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6243903054:AAGxLIvpbt_v9oySQG2FZ14HVvfSO2cI6I4")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "")                             
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")  
+    OWNER_ID = os.environ.get("OWNER_ID", "5149183428")                             
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://projectnv:projectnv321@cluster0.wieiqcz.mongodb.net/?retryWrites=true&w=majority")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     SESSION = os.environ.get("SESSION", "")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001885467323"))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "@nancyB_bot")
 
 
 def LOGGER(name: str) -> logging.Logger:
